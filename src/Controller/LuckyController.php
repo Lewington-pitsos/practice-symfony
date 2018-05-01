c10cb264be72fbd8cff6cf85129be83106ece1af
Louka Ewington-Pitsos <louka.ewington-pitsos@estimateone.com>
2018-05-01 15:41:22 +1000

    set /lucky/number route to use annotation syntax
      - The route specification is removed from config/routes.yaml
      - The 'annotations' packages is required by composer
      - Using the annotation syntax in src/Controller/LuckyController, a route is
      added at /lucky/number for that controller

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,10 +1,16 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
+
+ /**
+ * @Route("/lucky/number")
+ */
     public function number()
     {
         $number = mt_rand(0, 100);
